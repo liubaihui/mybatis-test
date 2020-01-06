03a2a0f2624d25da4260a06f36291e19d025b314
liubh4 <abc123@A>
2020-01-06 16:41:20 +0800

    初始化mybatis项目

---
+++ src/test/java/com/example/exampleMybatis/ExampleMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exampleMybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
